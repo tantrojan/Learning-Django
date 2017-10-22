edeca4307e5f30fed3396d397aaed590a51a10e8
tantrojan <tg.99ghosh@gmail.com>
2017-10-23 00:31:36 +0530

    Connecting to the database from views

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /blog/
     url(r'^$', views.index , name='index'),
+    # /blog/345/
     url(r'^(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
-    return HttpResponse("<h1>This is a blog page</h1>")
+    all_posts=Post.objects.all()
+    html = ''
+    for post in all_posts:
+        url =str(post.id) + '/'
+        html+='<a href="'+ url+ '">'+post.title+'</a><br>'
+    return HttpResponse(html)
+
 
 def detail(request,post_id):
     return HttpResponse("<h1>Post no :"+str(post_id)+"</h1>")
