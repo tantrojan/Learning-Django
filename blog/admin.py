afc850ea0692685a252ad5ab121f738d7f4b8dee
tantrojan <tg.99ghosh@gmail.com>
2017-10-22 22:10:15 +0530

    unwanted lines removed due to merging

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from .models import Post
 
-<<<<<<< HEAD
+
 admin.site.register(Post)
-=======
-admin.site.register(Post)
->>>>>>> 3f98e4b092cb85cb5908ebe38934bb1db28cc213
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.http import HttpResponse
 
 def index(request):
@@ -6,8 +5,3 @@ def index(request):
 
 def detail(request,post_id):
     return HttpResponse("<h1>Post no :"+str(post_id)+"</h1>")
-=======
-from django.shortcuts import render
-
-# Create your views here.
->>>>>>> 3f98e4b092cb85cb5908ebe38934bb1db28cc213
