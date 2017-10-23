1b6506d038fd1c4ada2f157540dd53b8a7c95476
tantrojan <tg.99ghosh@gmail.com>
2017-10-23 15:10:09 +0530

    Dynamic Data in template

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Post
 
 def index(request):
-    all_posts=Post.objects.all()
-    html = ''
-    for post in all_posts:
-        url =str(post.id) + '/'
-        html+='<a href="'+ url+ '">'+post.title+'</a><br>'
-    return HttpResponse(html)
+    posts=Post.objects.all()
+    return render(request,"blog\index.html", {'posts':posts})
 
 
 def detail(request,post_id):
